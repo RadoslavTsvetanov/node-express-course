0999ca5304b9dc945b1dc6382b5cd77de991677b
StalinGaming <lo_ol@abv.bg>
2023-02-28 15:55:54 +0200

    add basic structure for server

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,24 @@
-console.log('04 Store API')
+require("dotenv").config();
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const express = require("express");
+const app = express();
+port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send('<h1>hi</h1><a href = "/api/v1/products">products</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    //connectDB
+    app.listen(port);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
